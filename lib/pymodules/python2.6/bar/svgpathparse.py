35c13c65ff07303581a266e35c0fae016eda8206
pmajka <pmajka@bf95b417-27cd-4b1f-9e77-6486572a7b41>
2011-09-15 20:09:45 +0000

    Adding svgpathparse.py licencing information
    git-svn-id: http://svn.3dbar.org/svn/3dbar_sharable/trunk@29 bf95b417-27cd-4b1f-9e77-6486572a7b41

--- lib/pymodules/python2.6/bar/svgpathparse.py
+++ lib/pymodules/python2.6/bar/svgpathparse.py
@@ -23,6 +23,25 @@
 ###############################################################################
 
 """
+Extracted from: simplepath.py
+functions for digesting paths into a simple list structure
+
+Copyright (C) 2005 Aaron Spike, aaron@ekips.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 http://www.ekips.org/comp/inkscape/parsepath.php
 ekips path parsing code
 
