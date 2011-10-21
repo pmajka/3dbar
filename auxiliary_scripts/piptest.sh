afabd9573dde3ebadd76dc6b9a4d5461281299dd
jkowalski <jkowalski@bf95b417-27cd-4b1f-9e77-6486572a7b41>
2011-10-21 11:28:55 +0000

    piptest.sh names output files properly
    git-svn-id: http://svn.3dbar.org/svn/3dbar_sharable/trunk@69 bf95b417-27cd-4b1f-9e77-6486572a7b41

--- auxiliary_scripts/piptest.sh
+++ auxiliary_scripts/piptest.sh
@@ -8,7 +8,7 @@ do
     python ${SCRIPT_FNAME} ${file} > temp.dot
     dot -T png -o temp.png temp.dot
     
-    ofn=`basename ${file} | cut -f1 -d.`
-    mv temp.png $1/${ofn}.png
+    ofn=`basename ${file}`
+    mv temp.png $1/${ofn%.*}.png
     rm temp.dot
 done
