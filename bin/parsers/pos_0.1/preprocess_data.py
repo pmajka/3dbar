e948060fd05eb35b8a532abe3a5b89c8d44e4e64
pmajka <pmajka@bf95b417-27cd-4b1f-9e77-6486572a7b41>
2013-09-05 14:49:37 +0000

    Starting with parser for possum template delineation.
    git-svn-id: http://svn.3dbar.org/svn/3dbar_sharable/trunk@259 bf95b417-27cd-4b1f-9e77-6486572a7b41

--- bin/parsers/pos_0.1/preprocess_data.py
+++ bin/parsers/pos_0.1/preprocess_data.py
@@ -1,25 +1,33 @@
-import os
+import os, sys
 import csv
 
-def to_rgb(r,g,b):
+def to_rgb(r ,g, b):
     return '%02x%02x%02x' % (r, g, b)
 
-ifile  = open('label_descriptions.txt', "rb")
-reader = csv.reader(ifile, delimiter=' ', quotechar='"', quoting=csv.QUOTE_ALL)
+label_desc_file = sys.argv[1]
+output_fullname_file = sys.argv[2]
+label_desc  = open(label_desc_file, "rb")
+csv_reader = csv.reader(label_desc, delimiter=' ', quotechar='"', quoting=csv.QUOTE_ALL)
 
 results = []
 
-for row in reader:
+for row in csv_reader:
     row = filter(None, row)
-    lid = int(row[0])
-    r,g,b = tuple(map(int, row[1:4]))
-    n = row[7].replace('"',"")
-    abbr = n[0:2]
-    if lid < 100:
-        print lid, abbr, to_rgb(r,g,b), n
-        results.append([to_rgb(lid,lid,lid), abbr, to_rgb(r,g,b), n])
 
-w = file('fullnames.txt','w')
-for l in results:
-    wstr = "\t".join(map(str, l)) + "\n"
-    w.write(wstr)
+    # Skip lines which are comments
+    if row[0][0] != "#":
+        label_id = int(row[0])
+        r, g, b = tuple(map(int, row[1:4]))
+        fullname = row[7].replace('"', "")
+        abbreviation = fullname[0:2]
+
+        # Remvoe labels above 100 as they are not related
+        # to anatomy
+        if label_id < 100:
+            results.append([to_rgb(label_id, label_id, label_id),
+                            abbreviation, to_rgb(r,g,b), fullname])
+
+writer = file(output_fullname_file, 'w')
+for line in results:
+    writer_string = "\t".join(map(str, line)) + "\n"
+    writer.write(writer_string)
