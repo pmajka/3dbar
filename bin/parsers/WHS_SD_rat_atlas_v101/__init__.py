77fb2d72af860cbd5f85b3f565714d4d1ba0f416
Piotr Majka <pmajka@nencki.gov.pl>
2016-01-14 13:49:05 +1100

    ENCH: Adding new parser: WHS_SD_rat_atlas_v101

---
+++ bin/parsers/WHS_SD_rat_atlas_v101/__init__.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#    This file is part of 3d Brain Atlas Reconstructor                        #
+#                                                                             #
+#    Copyright (C) 2010-2012 Piotr Majka, Jakub M. Kowalski                   #
+#                                                                             #
+#    3d Brain Atlas Reconstructor is free software: you can redistribute      #
+#    it and/or modify it under the terms of the GNU General Public License    #
+#    as published by the Free Software Foundation, either version 3 of        #
+#    the License, or (at your option) any later version.                      #
+#                                                                             #
+#    3d Brain Atlas Reconstructor is distributed in the hope that it          #
+#    will be useful, but WITHOUT ANY WARRANTY; without even the implied       #
+#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.         #
+#    See the GNU General Public License for more details.                     #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along  with  3d  Brain  Atlas  Reconstructor.   If  not,  see            #
+#    http://www.gnu.org/licenses/.                                            #
+#                                                                             #
+###############################################################################
+
+import sys
+import os
+from PIL import Image, ImageChops
+import nifti
+import numpy as np
+
+from bar.mbatParser import barMBATParser
+from data import *
+
+class AtlasParser(barMBATParser):
+    """
+    Required to implement:
+    _getSourceImage(self, slideNumber)
+    _createMask(self, image, colorValue)
+    _getZCoord(self, slideNumber)
+    _getSpatialTransfMatrix(self, slideNumber)
+    _getNewPathID(self, structName = None)
+    """
+    _requiredInternalData=barMBATParser._requiredInternalData +\
+            ['_volume','_pathNumber']
+
+    def __init__(self, inputAtlasFile, outputDirectory, **kwargs):
+        # Extend passed properties and invoke uper class constuctor
+        kwargs.update(atlasparserProperties)
+        barMBATParser.__init__(self, inputAtlasFile, outputDirectory, **kwargs)
+
+        # Fix parrents for empty groups as in mbat atlases have multiple
+        # hierarchy root elements.
+        self.indexer.hierarchyRootElementName = C_HIERARCHY_ROOT_ELEMENT_ABBREV
+        for (k,v) in self.parents.iteritems():
+            if v == "": self.parents[k] =\
+                    self.indexer.hierarchyRootElementName
+
+        # Define hierarchy, fullNameMapping, and color mapping
+        self.indexer.hierarchy       = self.parents
+        self.indexer.fullNameMapping = self.fullNameMapping
+        self.indexer.colorMapping    = self.structureColours
+
+        # Fix the full name of the root element name
+        self.indexer.groups[C_HIERARCHY_ROOT_ELEMENT_ABBREV].fullname = \
+                C_HIERARCHY_ROOT_ELEMENT_FULLNAME
+
+        # Set indexer properties, but get indexer reference matrix in advance.
+        indexerProperties['RefCords'] = ",".join(map(str, self._getIndexerRefMatrix()))
+        self.indexer.updateProperties(indexerProperties)
+
+    def _getSpatialCoordinate(self, voxelIndexTuple):
+        if self._volumeSrc.header['sform_code']: spFunct = self._volumeSrc.vx2s
+        if self._volumeSrc.header['qform_code']: spFunct = self._volumeSrc.vx2q
+        try:
+            return spFunct(voxelIndexTuple)
+        except:
+            raise NotImplementedError, "unable to fetch zVoxelIndex via any known method"
+
+    def _getZCoord(self, slideNumber):
+        zVoxelIndex = self.slideRange[slideNumber]
+        return self._getSpatialCoordinate((0, zVoxelIndex, 0))[1]
+
+    def _getSpatialTransfMatrix(self, slideNumber):
+        # tip:  cc : cornerCoords
+        #
+        # Usually I'll read the coordinates based on the information
+        # from the niftii file and this will give a proper spatial
+        # transformation matrix with an accuracy tp to 1/2 voxel size
+        # during reconstruction. Here, however, I have manually tweaked
+        # values which gives at least 1/4 voxel accuracy:
+
+        # The initial version for further reference:
+        # cc = self._getSpatialCoordinate((0, 0, 511))
+        # return (self._voxelSize, cc[0], -self._voxelSize, cc[2])
+
+        a,b,c,d = spatialTransformationMatrix
+        return (self._voxelSize, a, -self._voxelSize, d)
+
+    def _defineSlideRange(self, antPostAxis = 2):
+        antPostDim = self._volumeHeader['dim'][antPostAxis]
+        self._voxelSize = self._volumeHeader['pixdim'][antPostAxis]
+
+        # Reverse direction of the slides along A-P axis just to have
+        # anterior on first slides and posteror on the latter.
+        self.slideRange = map(lambda x: (antPostDim - 1) - x, range(0, antPostDim))
+
+    def _getSourceImage(self, slideNumber):
+        volumeSlide = self._volume[:, self.slideRange[slideNumber], :]
+        volumeSlide[ volumeSlide[:,:] == 0 ] = 0
+
+        m = 30  # The segmentation provided by the authors of the paper is
+                # great, but since it has been prepared manually, it has some
+                # random voxels close to the edgs of the image segmented.
+                # this cause the reconstructor to create a very wired
+                # reconstructions. Here erase 40 voxels of each side of the
+                # volume to wipe out such unwanted segmentation. This does not
+                # cause any actual segmentation to be clipped so it's safe.
+        volumeSlide[0:m, :] = 0
+        volumeSlide[-m:, :] = 0
+        volumeSlide[:, 0:m] = 0
+        volumeSlide[:, -m:] = 0
+
+        image = Image.fromarray(volumeSlide.astype(np.uint8), 'L').convert("RGB")
+        image = image.transpose(Image.FLIP_TOP_BOTTOM).convert("RGB")
+        return image
+
+    def parse(self, slideNumber):
+        tracedSlide = barMBATParser.parse(self, slideNumber,\
+                                                 generateLabels=True,
+                                                 useIndexer=False,
+                                                 writeSlide=False)
+
+        # Natalia wants labels to have smaller font size as (in her opinion)
+        # they are hard to decipher.
+        map(lambda x: \
+                x._attributes.update({'font-size':'10px'}),\
+                tracedSlide.labels)
+        tracedSlide.writeXMLtoFile(self._getOutputFilename(slideNumber))
+
+        return tracedSlide
+
+    def reindex(self):
+        barMBATParser.reindex(self)
+        self.indexer.hierarchy       = self.parents
+        self.indexer.fullNameMapping = self.fullNameMapping
+        self.indexer.colorMapping    = self.structureColours
+
+        # Fix the full name of the root element name
+        self.indexer.groups[C_HIERARCHY_ROOT_ELEMENT_ABBREV].fullname = \
+                C_HIERARCHY_ROOT_ELEMENT_FULLNAME
+
+        self.writeIndex()
+
+
+if __name__=='__main__':
+    try:
+        inputDirectory  = sys.argv[1]
+        outputDirectory = sys.argv[2]
+    except:
+        inputDirectory  = \
+        'atlases/WHS_SD_rat_atlas_v101/src/MBAT_WHS_SD_rat_atlas_v1.01/start_atlas/WHS_SD_rat_atlas_v1.01.atlas'
+        outputDirectory = 'atlases/WHS_SD_rat_atlas_v101/caf/'
+
+    ap = AtlasParser(inputDirectory, outputDirectory)
+    ap.parseAll()
+    ap.reindex()
---
+++ bin/parsers/WHS_SD_rat_atlas_v101/data.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#    This file is part of 3d Brain Atlas Reconstructor                        #
+#                                                                             #
+#    Copyright (C) 2010-2012 Piotr Majka, Jakub M. Kowalski                   #
+#                                                                             #
+#    3d Brain Atlas Reconstructor is free software: you can redistribute      #
+#    it and/or modify it under the terms of the GNU General Public License    #
+#    as published by the Free Software Foundation, either version 3 of        #
+#    the License, or (at your option) any later version.                      #
+#                                                                             #
+#    3d Brain Atlas Reconstructor is distributed in the hope that it          #
+#    will be useful, but WITHOUT ANY WARRANTY; without even the implied       #
+#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.         #
+#    See the GNU General Public License for more details.                     #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along  with  3d  Brain  Atlas  Reconstructor.   If  not,  see            #
+#    http://www.gnu.org/licenses/.                                            #
+#                                                                             #
+###############################################################################
+
+import datetime
+
+CONF_PARSER_COMMENT = """CAF dataset based on: \
+<a href="http://software.incf.org/software/waxholm-space-atlas-of-the-sprague-dawley-rat-brain/waxholm-space-atlas-of-the-sprague-dawley-rat-brain-package/v1.01/mbat-ready-sprague-dawley-atlas-v1.01/at_download/file" target="_blank">\
+Waxholm Space atlas of the Sprague Dawley rat brain</a>: \
+Papp, E.A., Leergaard, T.B., Calabrese, E., Johnson, G.A., & Bjaalie, J.G. (2014). <i>NeuroImage Waxholm Space atlas of the Sprague Dawley rat brain</i>. NeuroImage, 97, 374–386. http://doi.org/10.1016/j.neuroimage.2014.04.001"""
+
+CONF_PARSER_NAME    = 'WHS_SD_rat_atlas_v101'
+CONF_CONTACT_COMMENT= 'Piotr Majka, Nencki Institute of Experimental Biology'
+CONF_CONTACT_EMAIL  = 'pmajka@nencki.gov.pl'
+CONF_CAF_COMPIL_TIME= datetime.datetime.utcnow().strftime("%F %T")
+CONF_CAF_FULL_NAME = 'Waxholm Space atlas of the Sprague Dawley rat brain, version 1.01'
+
+REFERENCE_WIDTH = 512
+REFERENCE_HEIGHT = 512
+
+# The values below are tweaked values which are automatically
+# extrated from the niftii file, but these are slightly modified
+# for better reconstruction accuracy
+# a * x + b, c * y + d
+voxelSize = 0.0390625
+a, b, c, d = voxelSize, -9.53240414, -voxelSize, 10.31178775
+spatialTransformationMatrix = (a,b,c,d)
+alignerCoordinateTuple = (b,d,a,c)
+
+C_HIERARCHY_ROOT_ELEMENT_ABBREV = 'Brain-region-hierarchy'
+C_HIERARCHY_ROOT_ELEMENT_FULLNAME = 'Brain region hierarchy'
+
+tracedSlideTemplate = """<?xml version="1.0" ?><svg baseProfile="full" height="%d" id="body"
+preserveAspectRatio="none" version="1.1" viewBox="0 0 %d %d"
+width="%d" xmlns="http://www.w3.org/2000/svg"
+xmlns:ev="http://www.w3.org/2001/xml-events"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+xmlns:bar="http://www.3dbar.org">
+
+<title></title>
+<desc></desc>
+
+<defs></defs>
+
+<g id='content'>
+</g>
+
+</svg>
+""" % (REFERENCE_HEIGHT, REFERENCE_WIDTH, REFERENCE_HEIGHT, REFERENCE_WIDTH)
+
+filenameTempates = dict(traced='%d_traced_v%d.svg')
+
+renderingProperties = {}
+renderingProperties['ReferenceWidth']  = REFERENCE_WIDTH
+renderingProperties['ReferenceHeight'] = REFERENCE_HEIGHT
+renderingProperties['imageSize']       = (REFERENCE_WIDTH*8, REFERENCE_HEIGHT*8)
+
+potraceProperties    = {}
+potraceProperties['potrace_accuracy_parameter']   ='0.001'
+potraceProperties['potrace_svg_resolution_string']='300x300'
+potraceProperties['potrace_width_string']   =      '%dpt' % REFERENCE_WIDTH
+potraceProperties['potrace_height_string']  =      '%dpt' % REFERENCE_HEIGHT
+
+tracerSettings={}
+tracerSettings['DumpEachStepSVG']          = False
+tracerSettings['DumpEachStepPNG']          = False
+tracerSettings['DumpWrongSeed']            = True
+tracerSettings['DumpVBrain']               = False
+tracerSettings['DumpDirectory']            = '.'
+tracerSettings['DetectUnlabelled']         = False
+tracerSettings['CacheLevel']               = 5
+tracerSettings['MinFiterTimesApplication'] = 3
+tracerSettings['GrowDefaultBoundaryColor'] = 200
+tracerSettings['RegionAlreadyTraced']      = 100
+tracerSettings['UnlabelledTreshold']       = 500
+tracerSettings['PoTraceConf'] = potraceProperties
+tracerSettings['NewPathIdTemplate'] = 'structure%d_%s_%s'
+
+atlasparserProperties=[
+('backgroundColor', (0, 0, 0)),
+('filenameTemplates', filenameTempates),
+('renderingProperties', renderingProperties),
+('tracingProperties', tracerSettings),
+('slideTemplate', tracedSlideTemplate)]
+
+indexerProperties = dict([
+('Genus', 'Rattus'),
+('Species', 'Rattus norvegicus'),
+('Strain', 'Sprague Dawley'),
+('Age', 'adult, postnatal day 80, mass: 397.6g'),
+('Sex', 'male'),
+('Source', 'http://software.incf.org/software/waxholm-space-atlas-of-the-sprague-dawley-rat-brain/waxholm-space-atlas-of-the-sprague-dawley-rat-brain-package/v1.01/mbat-ready-sprague-dawley-atlas-v1.01/at_download/file'),
+('Language', 'En'),
+('Licencing', '<a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/deed.pl" target="_blank"><img alt="CC-BY-NC-SA" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>'),
+('SourceLicencing', ' CC-BY-NC-SA (<a href="http://software.incf.org/software/waxholm-space-atlas-of-the-sprague-dawley-rat-brain" target="_blank">see details</a>)'),
+('ReferenceWidth', str(REFERENCE_WIDTH)),
+('ReferenceHeight', str(REFERENCE_HEIGHT)),
+('FilenameTemplate',filenameTempates['traced']),
+('CAFSlideOrientation', 'coronal'),
+('CAFSlideUnits', 'mm'),
+('CAFName', CONF_PARSER_NAME),
+('CAFFullName', CONF_CAF_FULL_NAME),
+('CAFComment', CONF_PARSER_COMMENT),
+('CAFCreator', CONF_CONTACT_COMMENT),
+('CAFCreatorEmail', CONF_CONTACT_EMAIL),
+('CAFCompilationTime',CONF_CAF_COMPIL_TIME),
+('CAFAxesOrientation', 'RSA')])
